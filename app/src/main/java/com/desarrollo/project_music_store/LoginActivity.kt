e024f38b31822b2a6d8e9608865ba5ea9b58aa54
djackgongora <dacho4122@gmail.com>
2021-10-14 17:03:07 -0500

    Agregando login

--- app/src/main/java/com/desarrollo/project_music_store/LoginActivity.kt
+++ app/src/main/java/com/desarrollo/project_music_store/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_login)
     }
 }
