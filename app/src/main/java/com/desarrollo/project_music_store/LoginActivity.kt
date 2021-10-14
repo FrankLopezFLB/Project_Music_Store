3f6bae9f8ed2238a3b03fd6a4145f02a1956a1e5
djackgongora <dacho4122@gmail.com>
2021-10-14 14:33:01 -0500

    Creando activityLogin, vie_log, y añadiendo diseño[top]

---
+++ app/src/main/java/com/desarrollo/project_music_store/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.desarrollo.project_music_store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
