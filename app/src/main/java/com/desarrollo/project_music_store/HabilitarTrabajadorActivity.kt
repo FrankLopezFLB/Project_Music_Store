51a902fd66071b44f652d0c7a09561a42fcda5b8
Green <franklopezflb@gmail.com>
2021-10-15 21:27:14 -0500

    disenos terminados

--- app/src/main/java/com/desarrollo/project_music_store/HabilitarTrabajadorActivity.kt
+++ app/src/main/java/com/desarrollo/project_music_store/HabilitarTrabajadorActivity.kt
@@ -1,20 +1,11 @@
 package com.desarrollo.project_music_store
 
-import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class HabilitarTrabajadorActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_habilitar_trabajador)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 }
