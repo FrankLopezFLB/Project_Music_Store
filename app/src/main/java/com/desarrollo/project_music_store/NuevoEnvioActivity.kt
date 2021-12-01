3007585554e3712a4a7b34822a5c3a00684c87f8
jossrc <kuro.rc10@gmail.com>
2021-12-01 03:19:39 -0500

    Registro de Envíos

--- app/src/main/java/com/desarrollo/project_music_store/NuevoEnvioActivity.kt
+++ app/src/main/java/com/desarrollo/project_music_store/NuevoEnvioActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Spinner
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_nuevo_envio.*
 import org.json.JSONArray
+import java.lang.Exception
 import java.util.ArrayList
 
 class NuevoEnvioActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
@@ -37,10 +38,85 @@ class NuevoEnvioActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
         })
 
         hilo2.start()
+
     }
 
     fun grabarEnvio(v: View) {
-        Log.wtf("e", "Prueba")
+
+        try {
+            var descripcion = edtDescripcionEnvioRegistrar.text.toString().trim()
+
+            if (descripcion.isNullOrEmpty()) {
+                showToast("El campo descripción es obligatorio")
+                return;
+            }
+
+            var idProd = spProductosEnvioRegistrar.selectedItemPosition
+
+            if (idProd == 0) {
+                showToast("Seleccione un Producto válido")
+                return;
+            }
+
+            var idCli = spClientesEnvioRegistrar.selectedItemPosition
+
+            if (idCli == 0) {
+                showToast("Seleccione un Cliente Válido")
+                return;
+            }
+
+            var direccion = edtDireccionEnvioRegistrar.text.toString().trim()
+
+            if (direccion.isNullOrEmpty()) {
+                showToast("El campo descripción es obligatorio")
+                return;
+            }
+
+            var distrito = edtDistritoEnvioRegistrar.text.toString().trim()
+
+            if (distrito.isNullOrEmpty()) {
+                showToast("El campo distrito es obligatorio")
+                return;
+            }
+
+            var strCantidad = edtCantidadEnvioRegistrar.text.toString().trim()
+
+            if (strCantidad.isNullOrEmpty()) {
+                showToast("El campo cantidad es obligatorio")
+                return;
+            }
+
+            var cantidad = strCantidad.toInt()
+
+            if (cantidad <= 0) {
+                showToast("La cantidad debe ser un número mayor a cero")
+                return;
+            }
+
+            var parametros = "?xdescripcion=${descripcion}&xidprod=${idProd}&xidcli=${idCli}&xdireccion=${direccion}&xdistrito=${distrito}&xcantidad=${cantidad}"
+
+            var rutaServicio = "http://192.168.18.5:8085/servicio/servicioEnvio/nuevo_envio.php${parametros}"
+
+            var hilo = Thread(Runnable {
+                var rpta = Utilitario.enviarDatos_String(rutaServicio)
+
+                runOnUiThread {
+                    if (rpta == "ok") {
+                        clear()
+                        showToast("Nuevo Envío realizado")
+                    } else {
+                        showToast("Hubo problemas al generar el envío")
+                    }
+
+                }
+            })
+
+            hilo.start()
+
+        } catch (err: Exception) {
+            showToast("Error inesperado, verifique que todos los campos estén correctamente ingresados")
+        }
+
     }
 
     private fun loadProductoSpinner(cadena: String) {
@@ -88,6 +164,7 @@ class NuevoEnvioActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
         TODO("Not yet implemented")
     }
 
@@ -100,7 +177,6 @@ class NuevoEnvioActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
     }
 
     private fun clear() {
-        edtCodigoEnvioRegistrar.text = null;
         edtDescripcionEnvioRegistrar.text = null;
         spProductosEnvioRegistrar.setSelection(0)
         spClientesEnvioRegistrar.setSelection(0)
